6236e2aca21d4507180e3832bca2126d3517ccc0
johnnysee <js.johnny.see@gmail.com>
2021-10-15 23:53:38 +0200

    Added AddUser.module.css and pass in classes.card and props.className into Card.jsx

--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -1,21 +1,23 @@
 import React from "react";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
-  // This function adds an event listener that when onSubmit, it will not erase the input values
+  // This function adds an event listener that when onSubmit, it will not erase the input values, not reloading page.
   const addUserHandler = (event) => {
     event.preventDefault();
   };
 
   return (
-    <div onSubmit={addUserHandler}>
-      <form>
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
         <label htmlFor>Username</label>
         <input id="username" type="text" />
         <label htmlFor>Age (Years)</label>
         <input id="age" type="number" />
         <button type="submit">Add User</button>
       </form>
-    </div>
+    </Card>
   );
 };
 
